d6eb606c1c342467cfb157b9aea87ca93c5a726b
Laysan <Laysansh@gmail.com>
2023-10-03 16:28:05 -0600

    Add new solution

---
+++ src/main/java/SupermarketQueue.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class SupermarketQueue {
+
+    public static int solveSuperMarketQueue(int[] customers, int n) {
+
+        int[] checkouts = new int[n];
+
+        for (int customer : customers){
+            checkouts[0] += customer;
+            Arrays.sort(checkouts);
+        }
+
+        System.out.println(Arrays.toString(checkouts));
+
+        return checkouts[n - 1];
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(solveSuperMarketQueue(new int[]{5,3,4}, 1));
+        System.out.println(solveSuperMarketQueue(new int[]{10,2,3,3}, 2));
+        System.out.println(solveSuperMarketQueue(new int[]{2,3,10}, 2));
+        System.out.println(solveSuperMarketQueue(new int[]{109, 110, 100, 94, 105, 106, 106}, 7));
+    }
+}
