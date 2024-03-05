d09e230db8a93ecbf63309a360817cd7008a4759
laysan <laysansh@gmail.com>
2024-03-05 11:52:10 -0700

    max in array

--- src/test/java/MaxInArrayTest.java
+++ src/test/java/MaxInArrayTest.java
@@ -1,3 +1,4 @@
+import jdk.jfr.Description;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -10,22 +11,23 @@ import java.util.stream.Stream;
 
 public class MaxInArrayTest {
 
-//    @Test
-//    public void testMaxInArray() {
-//
-//        double[] array1 = {1.1, 2.2, 8.8, 9.0};
-//        double[] array2 = {3.3, 4.4, 4.8, 12.0};
-//        double[] expectedResult = {12.0, 9.0, 4.8, 8.8};
-//
-//        Arrays.sort(expectedResult);
-//
-//        double[] actualResult = new MaxInArray().maxInArray(array1, array2);
-//
-//        Assert.assertEquals(actualResult, expectedResult);
-//
-//    }
+    @Test()
+    @Description("TestNG")
+    public void testMaxInArray() {
 
-        private static Stream<Arguments> masInArrayProvider() {
+        double[] array1 = {1.1, 2.2, 8.8, 9.0};
+        double[] array2 = {3.3, 4.4, 4.8, 12.0};
+        double[] expectedResult = {12.0, 9.0, 4.8, 8.8};
+
+        Arrays.sort(expectedResult);
+
+        double[] actualResult = MaxInArray.maxInArray(array1, array2);
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+    }
+
+        private static Stream<Arguments> maxInArrayProvider() {
 
             return Stream.of(
                     Arguments.of(new double[] {1.1, 2.2, 8.8, 9.0}, new double[] {3.3, 4.4, 4.8, 12.0}, new double[]{12.0, 9.0, 4.8, 8.8}),
@@ -35,8 +37,8 @@ public class MaxInArrayTest {
         }
 
         @ParameterizedTest
-        @MethodSource("masInArrayProvider")
-        public void testHappyNumber(double[] array1, double[] array2, double[] expectedResult) {
+        @MethodSource("maxInArrayProvider")
+        public void testMaxInArrayUp(double[] array1, double[] array2, double[] expectedResult) {
 
             Arrays.sort(expectedResult);
             double[] actual = MaxInArray.maxInArray(array1, array2);
